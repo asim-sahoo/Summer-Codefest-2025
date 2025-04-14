79f52ca81031548804ef1cac246a135136567c5b
asim-sahoo <asim2018off@gmail.com>
2025-04-14 23:17:35 +0530

    Fresh commit with all files deleted

--- Android App/Social/socialapp/lib/utils/utils.dart
+++
@@ -1,2 +0,0 @@
-// // filepath: d:\Files\Code\flutter_projects\Social\socialapp\lib\utils\utils.dart
-// export 'api_constants.dart';
--- Website/WellSta/Server/config.js
+++
@@ -1,4 +0,0 @@
-// Configuration for the server
-export const JWT_KEY = "mindfeed-secret-key-2025";
-export const MONGO_DB = "mongodb://localhost:27017/mindfeed";
-export const PORT = 4000;
